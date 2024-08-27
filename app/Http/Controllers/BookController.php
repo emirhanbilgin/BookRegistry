5969ab9576995df81116bcf8f189ae8e3ac8b122
emirhanbilgin <emirhan.bilgin@bahcesehir.edu.tr>
2024-08-27 13:53:26 +0300

    Eksik Route Model Binding düzeltmeleri ve attribute'ların array olarak gösterilmesi

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -26,10 +26,10 @@ class BookController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'title' => 'required',
-            'author' => 'required',
-            'isbn' => 'required|unique:books',
-            'cover_image' => 'nullable|image|mimes:jpg,png,jpeg|max:2048',
+            'title' => ['required'],
+            'author' => ['required'],
+            'isbn' => ['required', 'unique:books'],
+            'cover_image' => ['nullable', 'image', 'mimes:jpg,png,jpeg', 'max:2048'],
         ], [
             'isbn.unique' => 'Bu ISBN numarası daha önce kullanılmış lütfen başka bir numara giriniz.',
         ]);
@@ -54,22 +54,18 @@ class BookController extends Controller
         return view('books.show', compact('book'));
     }
 
-
-    public function edit($id)
+    public function edit(Book $book) // Route Model Binding kullanımı
     {
-        $book = Book::findOrFail($id);
         return view('books.edit', compact('book'));
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, Book $book) // Route Model Binding kullanımı
     {
-        $book = Book::findOrFail($id);
-
         $request->validate([
-            'title' => 'required',
-            'author' => 'required',
-            'isbn' => 'required|unique:books,isbn,' . $book->id,
-            'cover_image' => 'nullable|image|mimes:jpg,png,jpeg|max:2048',
+            'title' => ['required'],
+            'author' => ['required'],
+            'isbn' => ['required', 'unique:books,isbn,' . $book->id],
+            'cover_image' => ['nullable', 'image', 'mimes:jpg,png,jpeg', 'max:2048'],
         ], [
             'isbn.unique' => 'Bu ISBN numarası daha önce kullanılmış lütfen başka bir numara giriniz.',
         ]);
@@ -88,9 +84,8 @@ class BookController extends Controller
         return redirect()->route('books.index')->with('success', 'Kitap başarıyla güncellendi!');
     }
 
-    public function destroy($id)
+    public function destroy(Book $book) // Route Model Binding kullanımı
     {
-        $book = Book::findOrFail($id);
         $book->delete();
 
         return redirect()->route('books.index')->with('success', 'Kitap başarıyla silindi!');
